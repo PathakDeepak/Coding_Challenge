71c82f2e183bfcd64630f13dde9871a255fa0373
Pathak, Deepak <pathakdeepak024@gmail.com>
2022-01-03 21:50:47 +0530

    Adding first reversal array code

---
+++ src/Arrays/Reverse_Array.java
@@ -0,0 +1,46 @@
+package Arrays;
+
+public class Reverse_Array {
+
+    static void printArray(int arr[], int size){
+        for (int s = 0; s < size; s++) {
+            System.out.print(arr[s] + "\t");
+        }
+    }
+    // Complexity: O(n)
+     static void reverseArrayIterative(int arr[], int start, int end){
+        int temp;
+
+        while(start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+     }
+
+    // Complexity: O(n)
+    static void reverseArrayRecursively(int arr[], int start, int end){
+        int temp;
+
+        if(start>=end) {
+            return;
+        }
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArrayRecursively(arr, start+1, end-1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        printArray(arr, 6);
+        reverseArrayIterative(arr, 0, 5);
+        System.out.println("\nAfter reversal");
+        printArray(arr, 6);
+        reverseArrayRecursively(arr, 0, 5);
+        System.out.println("\nAgain recursively reversal");
+        printArray(arr, 6);
+    }
+}
